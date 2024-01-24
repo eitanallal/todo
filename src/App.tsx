602811d4144bfb715625bca1a45bea9d7d727fd2
eitanallal <allaleitan@gmail.com>
2024-01-24 10:24:49 +0200

    bug fixed. Able to see main screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,57 +3,84 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import styles from "./App.module.css";
 import { Category } from "./components/category/category";
-import { Item } from "./components/item/item";
 
-// interface ItemType {
-//   itemId: number;
-//   title: string;
-//   description: string;
+interface Item {
+  itemId: number;
+  title: string;
+  description: string;
+}
+
+// interface AppProps {
+//   handleAddItem: () => void;
 // }
 
 const App: React.FC = () => {
-  //   let nextToDoId = 3;
-  //   let nextDoneId = 0;
+  let nextToDoId = 3;
+  let nextDoneId = 3;
 
-  //   const [toDo, setToDo] = useState<ItemType[]>([
-  //     { itemId: 0, title: "Title 0", description: "Description todo 0" },
-  //     { itemId: 1, title: "Title 1", description: "Description todo 1" },
-  //     { itemId: 2, title: "Title 2", description: "Description todo 2" },
-  //   ]);
-  //   const [done, setDone] = useState<ItemType[]>([
-  //     // { itemId: 0, title: "Title 0", description: "Description done 0" },
-  //     // { itemId: 1, title: "Title 1", description: "Description done 1" },
-  //   ]);
+  const [toDo, setToDo] = useState<Item[]>([
+    { itemId: 0, title: "Title 0", description: "Description todo 0" },
+    { itemId: 1, title: "Title 1", description: "Description todo 1" },
+    { itemId: 2, title: "Title 2", description: "Description todo 2" },
+  ]);
+  const [done, setDone] = useState<Item[]>([
+    // { itemId: 0, title: "Title 0", description: "Description done 0" },
+    // { itemId: 1, title: "Title 1", description: "Description done 1" },
+  ]);
 
-  //   const handleAddItemToDo = (newTitle: string, newDescription: string) => {
-  //     setToDo([
-  //       ...toDo,
-  //       {
-  //         itemId: nextToDoId++,
-  //         title: "Title 3",
-  //         description: "Description 3",
-  //       },
-  //     ]);
-  //   };
+  const handleAddItemToDo = (newTitle: string, newDescription: string) => {
+    setToDo([
+      ...toDo,
+      {
+        itemId: nextToDoId++,
+        title: "Title 3",
+        description: "Description 3",
+      },
+    ]);
+  };
 
-  //   const handleAddItemDone = (newTitle: string, newDescription: string) => {
-  //     setDone([
-  //       ...done,
-  //       {
-  //         itemId: nextDoneId++,
-  //         title: "Title 3",
-  //         description: "Description 3",
-  //       },
-  //     ]);
-  //   };
+  const handleAddItemDone = (newTitle: string, newDescription: string) => {
+    setDone([
+      ...done,
+      {
+        itemId: nextToDoId++,
+        title: "Title 3",
+        description: "Description 3",
+      },
+    ]);
+  };
 
-  //   const handleDeleteItemToDo = (itemId: number) => {
-  //     setToDo(toDo.filter((i) => i.itemId !== itemId));
-  //   };
+  const handleEditItemToDo = (nextItem: any) => {
+    setToDo(
+      toDo.map((i) => {
+        if (i.itemId === nextItem.itemId) {
+          return nextItem;
+        } else {
+          return i;
+        }
+      })
+    );
+  };
 
-  //   const handleDeleteItemDone = (itemId: number) => {
-  //     setDone(done.filter((i) => i.itemId !== itemId));
-  //   };
+  const handleEditItemDone = (nextItem: any) => {
+    setDone(
+      done.map((i) => {
+        if (i.itemId === nextItem.itemId) {
+          return nextItem;
+        } else {
+          return i;
+        }
+      })
+    );
+  };
+
+  const handleDeleteItemToDo = (itemId: number) => {
+    setToDo(toDo.filter((i) => i.itemId !== itemId));
+  };
+
+  const handleDeleteItemDone = (itemId: number) => {
+    setDone(done.filter((i) => i.itemId !== itemId));
+  };
 
   return (
     <>
@@ -62,7 +89,7 @@ const App: React.FC = () => {
         <Category categoryTitle="DONE" categoryData={done}></Category>
         <button
           className={styles.circularButton}
-          onClick={() => Item handleAddItemToDo("Title 3", "Description 3")}
+          onClick={() => handleAddItemToDo("Title 3", "Description 3")}
         >
           <span className={styles.plusIcon}>+</span>
         </button>
--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,75 +1,19 @@
 import { useState } from "react";
 import styles from "./item.module.css";
 import { CheckCircle2 } from "lucide-react";
-// import handleAddItemDone from "../../App";
-
-interface ItemType {
-  itemId: number;
-  title: string;
-  description: string;
-}
+import handleAddItemDone from "../../App";
 
 interface ItemProps {
   itemTitle: string;
   itemDescription: string;
-  // handleAddItemToDo?: (newTitle: string, newDescription: string) => void;
-  // handleAddItemDone?: (newTitle: string, newDescription: string) => void;
-  // handleDeleteItemToDo?: (itemId: number) => void;
-  // handleDeleteItemDone?: (itemId: number) => void;
 }
 
 export const Item: React.FC<ItemProps> = ({ itemTitle, itemDescription }) => {
-  let nextToDoId = 3;
-  let nextDoneId = 0;
-
-  const [toDo, setToDo] = useState<ItemType[]>([
-    { itemId: 0, title: "Title 0", description: "Description todo 0" },
-    { itemId: 1, title: "Title 1", description: "Description todo 1" },
-    { itemId: 2, title: "Title 2", description: "Description todo 2" },
-  ]);
-  const [done, setDone] = useState<ItemType[]>([
-    // { itemId: 0, title: "Title 0", description: "Description done 0" },
-    // { itemId: 1, title: "Title 1", description: "Description done 1" },
-  ]);
-
-  const handleAddItemToDo = (newTitle: string, newDescription: string) => {
-    setToDo([
-      ...toDo,
-      {
-        itemId: nextToDoId++,
-        title: "Title 3",
-        description: "Description 3",
-      },
-    ]);
-  };
-
-  const handleAddItemDone = (newTitle: string, newDescription: string) => {
-    setDone([
-      ...done,
-      {
-        itemId: nextDoneId++,
-        title: "Title 3",
-        description: "Description 3",
-      },
-    ]);
-  };
-
-  const handleDeleteItemToDo = (itemId: number) => {
-    setToDo(toDo.filter((i) => i.itemId !== itemId));
-  };
-
-  const handleDeleteItemDone = (itemId: number) => {
-    setDone(done.filter((i) => i.itemId !== itemId));
-  };
-
   return (
     <>
       <div className={styles.smallRectangle}>
-        <button
-          className={styles.tickButton}
-          onClick={() => handleAddItemDone("hello", "hello")}
-        >
-          <CheckCircle2 color="grey" size={35} />
+        <button className={styles.tickButton} onClick={handleAddItemDone}>
+          <CheckCircle2 color="grey" size={22} />
         </button>
 
         <div className={styles.title}>{itemTitle}</div>
@@ -78,5 +22,3 @@ export const Item: React.FC<ItemProps> = ({ itemTitle, itemDescription }) => {
     </>
   );
 };
-
-export default Item;
